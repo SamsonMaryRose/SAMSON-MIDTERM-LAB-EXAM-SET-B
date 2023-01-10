21b12893fd44f5930148d85a2bb5e664e04da9ee
SamsonMaryRose <ms2777037@gmail.com>
2023-01-09 16:38:44 -0800

    updated

---
+++ exam.js
@@ -0,0 +1,19 @@
+const add = require ('./add')
+const read = require ('./read')
+
+const data = process.argv
+
+var note = {}
+
+if(data[2]) {
+    note = {
+        Employee: data[2],
+        Role: data[3],
+        Department: data[4], 
+    }
+    var oldNote = read()
+    add(note, oldNote)
+}
+if(data[2] == 'read'){
+    present(read())
+}
